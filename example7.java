fd31a0149e5ab88aaab3768db7d6d256d8f1eea8
Resham1458 <anuresham2015@gmail.com>
2017-12-31 17:54:26 -0500

    uploaded

---
+++ example7.java
@@ -0,0 +1,14 @@
+package HelloMaven.learnMaven;
+
+import org.testng.annotations.Test;
+
+public class example7 {
+	
+	@Test
+	public void message() {
+		
+	System.out.println("Welcome to Jenkins");
+	
+
+	}
+}
